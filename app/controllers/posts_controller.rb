482351152d2197067ab4584856bd4c6e19749c62
Dominic Pagtalunan <pagtalunan.d@gmail.com>
2022-01-20 23:15:45 +0800

    updated post,user and added index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,23 @@ before_action :authenticate_user!, only: [:new, :create]
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+        redirect_to action: :index
+    else
+    end
+
   end
 
   def index
+    @posts = current_user.posts
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title,:body)
   end
 
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_20_135822) do
+ActiveRecord::Schema.define(version: 2022_01_20_144910) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,4 +33,5 @@ ActiveRecord::Schema.define(version: 2022_01_20_135822) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
